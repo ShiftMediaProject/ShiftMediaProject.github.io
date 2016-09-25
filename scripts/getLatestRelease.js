8aed571febc4b823ec3f6dca9efc8536e64f7e65
Matt Oliver <protogonoi@gmail.com>
2016-09-25 15:55:30 +1000

    Add caching of javascript version lookup to prevent api limits.

--- scripts/getLatestRelease.js
+++ scripts/getLatestRelease.js
@@ -1,16 +1,52 @@
 $.fn.getLatestRelease = function(repo) {
     if( document.getElementById(repo+'ID') != null ) {
-        $.ajax( {
-            url : 'https://api.github.com/repos/ShiftMediaProject/'+repo+'/releases/latest',
-            dataType : 'jsonp',
-            success : function ( returndata ) {
-                $(returndata.data).each(function() {
-                    if(this.html_url.length > 0){
-                        var link = '<a href="'+this.html_url+'releases/latest">'+this.name+'</a>';
-                        $('#'+repo+'ID').html(link);
+        var hasStorage = ("sessionStorage" in window && window.sessionStorage),
+        storageKey = repo+'Version',
+        now, expiration, data = false;
+        try {
+            if (hasStorage) {
+                data = sessionStorage.getItem(storageKey);
+                if (data) {
+                    data = JSON.parse(data);
+                    now = new Date();
+                    expiration = new Date(data.timestamp);
+                    expiration.setMinutes(expiration.getMinutes() + 60);
+                    if (now.getTime() > expiration.getTime()) {
+                        data = false;
+                        sessionStorage.removeItem(storageKey);
                     }
-                });
+                }
             }
-        });
+        } catch (e) {
+            data = false;
+        }
+     
+        if (data) {
+            updateVersion(repo, data.html_url, data.name);
+        } else {
+            $.ajax( {
+                url : 'https://api.github.com/repos/ShiftMediaProject/'+repo+'/releases/latest',
+                dataType : 'jsonp',
+                success : function ( returndata ) {
+                    if (returndata.data.html_url.length > 0) {
+                        if (hasStorage) {
+                            try {
+                                sessionStorage.setItem(storageKey, JSON.stringify({
+                                    timestamp: new Date(),
+                                    html_url: returndata.data.html_url,
+                                    name: returndata.data.name
+                                }));
+                            } catch (e) {}
+                        }
+                        updateVersion(repo, returndata.data.html_url, returndata.data.name);
+                    }
+                }
+            });
+        }
     }
+};
+
+function updateVersion(repo, html_url, name) {
+    var link = '<a href="'+html_url+'/releases/latest">'+name+'</a>';
+    $('#'+repo+'ID').html(link);
 }
